3f787f4cd1cb32eff350bfa4222a26bffafc4cc1
root <root@Ademir.localdomain>
2022-08-28 18:29:24 -0300

    projeto base

---
+++ src/main/java/com/api/pautacontrol/PautacontrolApplication.java
@@ -0,0 +1,13 @@
+package com.api.pautacontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PautacontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PautacontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/pautacontrol/PautacontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.pautacontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PautacontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
